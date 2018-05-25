099227f2029b08ac039f1d44798059c1d057d4f8
Kiefer WIESSLER <kwiessle@e2r7p2.42.fr>
2018-05-25 17:45:01 +0200

    initial Push

---
+++ Controllers/Home.js
@@ -0,0 +1,5 @@
+const Home = (req, res) => {
+    res.render('Home')
+}
+
+module.exports = Home
---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const mustache = require('mustache-express')
+
+const PORT = 9999
+
+
+app.engine('html', mustache())
+app.set('view engine', 'html')
+app.set('Views', __dirname + '/Views/')
+app.use(express.static('public'))
+
+
+//Controllers
+
+const home = require('./Controllers/Home.js')
+app.get('/', home)
+
+
+
+var SERVER = http.createServer(app, (req, res) => {
+    res.writeHead(200)
+}).listen(PORT)
